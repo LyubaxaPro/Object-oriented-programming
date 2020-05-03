884010966332dd72a9368c59f46b2bbf74837b17
lyubaxapro <lyubaxova@mail.ru>
2020-05-03 14:01:05 +0300

    Add

---
+++ lab2/cmake-build-debug/CMakeFiles/oop_second.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/maksim/CLionProjects/sova/oop_second/list.cpp" "/home/maksim/CLionProjects/sova/oop_second/cmake-build-debug/CMakeFiles/oop_second.dir/list.cpp.o"
+  "/home/maksim/CLionProjects/sova/oop_second/main.cpp" "/home/maksim/CLionProjects/sova/oop_second/cmake-build-debug/CMakeFiles/oop_second.dir/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
